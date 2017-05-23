1f945d92e160d6032d6c57d3c6e64570ade999e7
lipeng <lipengb509@gmail.com>
2017-05-23 18:00:43 +0800

    Initial commit

---
+++ app/src/main/java/com/muzi/taobaotab/MainActivity.java
@@ -0,0 +1,53 @@
+package com.muzi.taobaotab;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private List<Fragment> fragmentList = new ArrayList<>();//fragment列表
+    private List<String> stringList = new ArrayList<>();//tab名的列表
+    private MyFragmentPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        getData();
+
+        for (int i = 0; i < stringList.size(); i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(stringList.get(i)));
+        }
+
+        adapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), fragmentList, stringList);
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void getData() {
+        fragmentList.add(new TypeFragment());
+        fragmentList.add(new TypeFragment());
+        fragmentList.add(new TypeFragment());
+        fragmentList.add(new TypeFragment());
+
+        stringList.add("16:00\n拍卖结束");
+        stringList.add("17:00\n疯狂抢购中");
+        stringList.add("18:00\n即将开始");
+        stringList.add("19:00\n即将开始");
+    }
+}
---
+++ app/src/main/java/com/muzi/taobaotab/TypeFragment.java
@@ -0,0 +1,10 @@
+package com.muzi.taobaotab;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by 李鹏 on 2017/5/23.
+ */
+
+public class TypeFragment extends Fragment {
+}
